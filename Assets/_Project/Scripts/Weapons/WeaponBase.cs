ca9629d001820ac7b2af925bb3b4a68631964158
Ioan Petcu <ipetcu_ipe@yahoo.ro>
2026-07-28 17:12:54 +0300

    chore: setup git lfs and upgrade unity version to 6.3 LTS
    - added standard unity .gitattributes for binary tracking
    - upgraded the project to 6000.3.20f1 (LTS)

--- Assets/_Project/Scripts/Weapons/WeaponBase.cs
+++ Assets/_Project/Scripts/Weapons/WeaponBase.cs
@@ -10,9 +10,9 @@ namespace Weapons
     {
         [SerializeField] protected float fireCooldown;
         [SerializeField] protected float altFireCooldown;
-        [SerializeField] public bool Firing { get; set; }
-        [SerializeField] public bool AltFiring { get; set; }
-        [SerializeField] public bool IsEnhanced { get; set; }
+        [field: SerializeField] public bool Firing { get; set; }
+        [field: SerializeField] public bool AltFiring { get; set; }
+        [field: SerializeField] public bool IsEnhanced { get; set; }
 
         protected float cooldownTo = -1;
         protected float timeLastShot = -1;
