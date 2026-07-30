035904fd0fba44301f1cd68186e405a48562bd54
Alex <citron440@gmail.com>
2026-07-30 13:21:30 +0300

    added some documentations, removed useless method

--- Assets/_Project/Scripts/StatusEffectSystem/StatusEffects/StatusEffect.cs
+++ Assets/_Project/Scripts/StatusEffectSystem/StatusEffects/StatusEffect.cs
@@ -12,6 +12,6 @@ public class StatusEffect
     public float TickTimer = -1f; // how often should OnTick proc
     public float TimeUntilNextTick;
     public virtual void OnTick() { } // an action that would happen multiple times ex: takedamage for damage over time effects
-    public virtual void OnApply() { }
-    public virtual void OnRemove() { }
+    public virtual void OnApply() { } // an action that would happen when applying the effect ex: decrease walkspeed by 10
+    public virtual void OnRemove() { } // an action that happens after the effect's duration has passes, usually just undoing the OnApply
 }
