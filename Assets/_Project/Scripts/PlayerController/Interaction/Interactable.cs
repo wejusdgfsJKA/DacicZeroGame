75e5d320d7b78834391d24b6e18461bcd1dc8a51
Ioan Petcu <ipetcu_ipe@yahoo.ro>
2026-08-02 15:45:18 +0300

    fix: resolve EventBus lifecycle and false errors in Interactable
    moved event registration from awake to onenable.
    
    fixed false-positive removebinding console errors on play mode exit.
    
    removed event listener wipes to preserve inspector bindings.

--- Assets/_Project/Scripts/PlayerController/Interaction/Interactable.cs
+++ Assets/_Project/Scripts/PlayerController/Interaction/Interactable.cs
@@ -1,22 +1,34 @@
 using EventBus;
 using UnityEngine;
 using UnityEngine.Events;
-namespace Interaction {
-    public class Interactable : MonoBehaviour {
+namespace Interaction
+{
+    public class Interactable : MonoBehaviour
+    {
         /// <summary>
         /// This fires when this object is interacted with.
         /// </summary>
         public UnityEvent<Transform> OnInteract;
-        protected void Awake() {
-            if (!EventBus<InteractionEvent>.AddActions(transform.GetInstanceID(), Interact)) {
+        
+        // CHANGED: Awake -> OnEnable so it re-subscribes if toggled off/on during gameplay.
+        protected void OnEnable()
+        {
+            if (!EventBus<InteractionEvent>.AddActions(transform.GetInstanceID(), Interact))
+            {
                 Debug.LogError($"{transform} unable to add actions to InteractionEvent bus!");
             }
         }
-        protected void OnDisable() {
+        
+        protected void OnDisable()
+        {
+            // CHANGED: removed error log. fails silently to prevent console spam when exiting Play Mode.
             EventBus<InteractionEvent>.RemoveBinding(transform.GetInstanceID());
-            OnInteract.RemoveAllListeners();
+            
+            // CHANGED: removed OnInteract.RemoveAllListeners() so it doesn't permanently delete Inspector setups.
         }
-        public void Interact(InteractionEvent @event) {
+        
+        public void Interact(InteractionEvent @event)
+        {
             OnInteract?.Invoke(@event.Interactor);
         }
     }
