4545deef94fa092cbce7459a50654c7f0fa5ddff
Alex <citron440@gmail.com>
2026-08-04 14:12:45 +0300

    added stationary pigeon enemy
    prefab for pigeonbot
    added a scannerbot component and a scantask for the pigeon and future stationary stealth enemies
    hated on navmeshagents cus they cant rotate without moving

---
+++ Assets/_Project/Scripts/AI/ScannerBotComponent.cs
@@ -0,0 +1,49 @@
+
+
+using UnityEngine;
+
+public class ScannerBotComponent : MonoBehaviour
+{
+    [SerializeField] public float AngularSpeed;
+    [SerializeField] public float TargetSwitchCooldown;
+    public float TargetSwitchCooldownTo;
+    public Vector3 TargetPosition;
+    bool isTurning;
+
+    void Update()
+    {
+        if (isTurning)
+        {
+            RotateTowardsTarget();
+        }
+        else if (TargetSwitchCooldownTo < Time.time)
+        {
+            isTurning = true;
+        }
+    }
+
+    void RotateTowardsTarget()
+    {
+        Vector3 dir = TargetPosition - transform.position;
+        if (ReachedTarget())
+        {
+            TargetSwitchCooldownTo = Time.time + TargetSwitchCooldown;
+            isTurning = false;
+        }
+
+        Quaternion targetRot = Quaternion.LookRotation(dir);
+
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation,
+            targetRot,
+            AngularSpeed * Time.deltaTime
+        );
+    }
+
+    public bool ReachedTarget()
+    {
+        Vector3 dir = TargetPosition - transform.position;
+        var angleDifference = Quaternion.Angle(transform.rotation, Quaternion.LookRotation(dir));
+        return angleDifference < 0.1f;
+    }
+}
