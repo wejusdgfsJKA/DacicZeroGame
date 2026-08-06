dde4eb3b94f3a34f8ce266f251fcbc7a339e71f9
Alex <citron440@gmail.com>
2026-08-06 15:37:27 +0300

    FANCIER HEALTH BAR
    GRAPHIC DESIGN IS MY PASSION

--- Assets/_Project/Scripts/HPSystem/HealthBarHandler.cs
+++ Assets/_Project/Scripts/HPSystem/HealthBarHandler.cs
@@ -9,16 +9,27 @@ namespace HP
     {
         [SerializeField] HPComponent EntityHpComponent;
         [SerializeField] Slider HealthBar;
+        [SerializeField] Slider FancyHealthBarBackground;
 
 
         private void Awake()
         {
             HealthBar.maxValue = EntityHpComponent.MaxHealth;
             HealthBar.value = EntityHpComponent.MaxHealth;
+
+            FancyHealthBarBackground.maxValue = EntityHpComponent.MaxHealth;
+            FancyHealthBarBackground.value = EntityHpComponent.MaxHealth;
         }
         private void OnEnable()
         {
             EntityHpComponent.OnDamageTaken += updateBar;
+        }
+
+        public void Update()
+        {
+            FancyHealthBarBackground.value = Mathf.Lerp(FancyHealthBarBackground.value, 
+                HealthBar.value, 
+                Mathf.Max(1.5f, (FancyHealthBarBackground.value + HealthBar.value)/2) * Time.deltaTime);
         }
 
         private void OnDisable()
