333372f748f6606efc65c3056a40956a23ec3227
AndrewThePanda1235 <andreipanda1235@gmail.com>
2026-08-12 16:41:45 +0300

    A* pathfinding
    added some scripts (not yet in use) for pathfinding for creating a grid over the first level scene and detecting obstacles to avoid

---
+++ Assets/_Project/Scripts/AI/Node.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class Node
+{
+    public bool walkable;
+    public Vector3 worldPosition;
+    public int gCost;
+    public int hCost;
+    
+    public int gridX;
+    public int gridY;
+
+    public Node parent;
+
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+
+    public Node(bool _walkable, Vector3 _worldPosition, int _gridX, int _gridY)
+    {
+        walkable = _walkable;
+        worldPosition = _worldPosition;
+        gridX = _gridX;
+        gridY = _gridY;
+    }
+}
